f89f84ef2dd23e2c2ef4a221928ae22647621210
Aanand Kainth <aanand@akainth.me>
2021-04-18 12:37:01 -0700

    Use an array of circles as the visualizer

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,76 @@
-const NUM_BANDS: i32 = 50;
+const NUM_BANDS: i32 = 35;
 
-const BAR_WIDTH: i32 = 2;
+const BAR_WIDTH: i32 = 5;
 const MARGIN: i32 = 8;
 
 const MAX_X: i32 = 2 * NUM_BANDS * (BAR_WIDTH + MARGIN) - MARGIN;
 
 fn main() {
-    println!("[Metadata]");
-    println!("Name=Pulse");
-    println!("Author=Aanand Kainth");
-    println!("Information=Shows an audio visualizer based on speaker output");
-    println!("License=MIT");
-    println!("Version=1.0.0");
-    println!();
+    println!("\
+    [Metadata]
+    Name=Pulse
+    Author=Aanand Kainth
+    Information=Shows an audio visualizer based on speaker output
+    License=MIT
+    Version=2.0.2
 
-    println!("[Rainmeter]");
-    println!("Update=40");
-    println!();
-
-    println!("[MeasureAudio]");
-    println!("Measure=Plugin");
-    println!("Plugin=AudioLevel");
-    println!("FFTSize=1024");
-    println!("FFTAttack=15");
-    println!("FFTDecay=250");
-    println!("Bands={}", NUM_BANDS);
-    println!();
+    [Rainmeter]
+    Update=40
+    SkinHeight=200
+    
+    [MeasureAudio]
+    Measure=Plugin
+    Plugin=AudioLevel
+    FFTSize=1024
+    FFTAttack=250
+    FFTDecay=250
+    Bands={}
+    ", NUM_BANDS);
 
     for i in 0..NUM_BANDS {
-        println!("[MeasureBand{}]", i);
-        println!("Measure=Plugin");
-        println!("Plugin=AudioLevel");
-        println!("Parent=MeasureAudio");
-        println!("Type=Band");
-        println!("BandIdx={}", i);
-        println!();
+        println!("\
+        [MeasureBand{0}]
+        Measure=Plugin
+        Plugin=AudioLevel
+        Parent=MeasureAudio
+        Type=Band
+        BandIdx={0}
+        ", i);
 
         let left_x = i * (BAR_WIDTH + MARGIN);
 
-        println!("[MeterBandLT{}]", i);
-        println!("Meter=Bar");
-        println!("MeasureName=MeasureBand{}", i);
-        println!("X={}", left_x);
-        println!("Y=0");
-        println!("W={}", BAR_WIDTH);
-        println!("H=100");
-        println!("BarColor=255,255,255");
-        println!("BarOrientation=Vertical");
-        println!();
+        println!("\
+        [MeterBandLT{0}]
+        Meter=Shape
+        Shape=Ellipse {1},(100+{2}-100 * [MeasureBand{0}]), ({2} * 100 * [MeasureBand{0}] / 100) | StrokeWidth 0
+        DynamicVariables=1
+        
+        ", i, left_x, BAR_WIDTH);
 
-        println!("[MeterBandLB{}]", i);
-        println!("Meter=Bar");
-        println!("MeasureName=MeasureBand{}", i);
-        println!("Flip=1");
-        println!("X={}", left_x);
-        println!("Y=100");
-        println!("W={}", BAR_WIDTH);
-        println!("H=100");
-        println!("BarColor=255,255,255");
-        println!("BarOrientation=Vertical");
-        println!();
+        println!("\
+        [MeterBandLB{0}]
+        Meter=Shape
+        Shape=Ellipse {1},(100+{2}+60 * [MeasureBand{0}]), ({2} * 100 * [MeasureBand{0}] / 130) | StrokeWidth 0
+        DynamicVariables=1
+        
+        ", i, left_x, BAR_WIDTH);
 
         let right_x = MAX_X - left_x;
 
-        println!("[MeterBandRT{}]", i);
-        println!("Meter=Bar");
-        println!("MeasureName=MeasureBand{}", i);
-        println!("X={}", right_x);
-        println!("Y=0");
-        println!("W={}", BAR_WIDTH);
-        println!("H=100");
-        println!("BarColor=255,255,255");
-        println!("BarOrientation=Vertical");
-        println!();
+        println!("\
+        [MeterBandRT{0}]
+        Meter=Shape
+        Shape=Ellipse {1},(100+{2}-100 * [MeasureBand{0}]), ({2} * 100 * [MeasureBand{0}] / 100) | StrokeWidth 0
+        DynamicVariables=1
+        
+        ", i, right_x, BAR_WIDTH);
 
-        println!("[MeterBandRB{}]", i);
-        println!("Meter=Bar");
-        println!("MeasureName=MeasureBand{}", i);
-        println!("Flip=1");
-        println!("X={}", right_x);
-        println!("Y=100");
-        println!("W={}", BAR_WIDTH);
-        println!("H=100");
-        println!("BarColor=255,255,255");
-        println!("BarOrientation=Vertical");
-        println!();
+        println!("\
+        [MeterBandRB{0}]
+        Meter=Shape
+        Shape=Ellipse {1},(100+{2}+60 * [MeasureBand{0}]), ({2} * 100 * [MeasureBand{0}] / 130) | StrokeWidth 0
+        DynamicVariables=1
+        
+        ", i, right_x, BAR_WIDTH);
     }
 }
